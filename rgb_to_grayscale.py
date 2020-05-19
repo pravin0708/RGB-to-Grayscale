998d47f84685463fcda50e8a7f22765be94a13c2
Pravin0708 <er.pravinpandit@gmail.com>
2020-05-19 20:06:10 +0530

    RGB picture to Graysacle Picture

---
+++ rgb_to_grayscale.py
@@ -0,0 +1,18 @@
+covert RGB to Grayscale
+
+# Import the modules from skimage
+from skimage import data, color
+
+# Load the rocket image
+rocket = data.rocket()
+
+# Convert the image to grayscale
+gray_scaled_rocket = color.rgb2gray(rocket)
+
+# Show the original image
+show_image(rocket,'Rockey image')
+
+# Show the grayscale image
+show_image(gray_scaled_rocket, 'Grayscale image')
+
+
